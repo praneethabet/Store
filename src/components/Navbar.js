932773dc7ba8303b71c4ed33635ba0925a8e6974
praneethabet <praneetha.ui6@gmail.com>
2020-05-12 14:55:34 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg';
 
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from Navbar</h3>
-            </div>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+                <Link to="/">
+                    <img src={logo} alt="store" className="navbar-brand"/>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link" className="textColor">Products</Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <button >
+                <i class="fas fa-cart-plus"/>
+                My Cart</button> 
+                </Link>
+           </nav>
         )
     }
 }
